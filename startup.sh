9d0f22767a65ace681c53da537eef31addf02472
Federico Lanzani <federico.lanzani@outlook.com>
2023-06-11 14:36:51 +0200

    test

--- startup.sh
+++ startup.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-# Install and configure X window with virtual screen
-apt-get install xserver-xorg libglu1-mesa-dev freeglut3-dev mesa-common-dev libxmu-dev libxi-dev
-
-# Configure the nvidia-x
-nvidia-xconfig -a --use-display-device=None --virtual=1280x1024
-
-# Run the virtual screen in the background (:0)
-/usr/bin/X :0 &
+apt-get install xvfb
 
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# We only need to setup the virtual screen once
-# Run the program with vitural screen
-DISPLAY=:0 streamlit run app/Home.py
 
-
-# If you dont want to type `DISPLAY=:0` everytime
-export DISPLAY=:0
-
-
+xvfb-run streamlit run app/Home.py
