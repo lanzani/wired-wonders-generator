ac6ac99afc43af4be7ed4d84d28e16e340b381dd
Federico Lanzani <federico@federicolanzani.com>
2023-07-13 11:02:45 +0200

    :rocket: Add chaos theory.

--- sketchbook/chaos_theory/strange_attractors.py
+++ sketchbook/chaos_theory/strange_attractors.py
@@ -1,14 +1,26 @@
+import math
 import random
+import time
+
 from matplotlib import pyplot as plt
 
-found = False
+attractors_to_save = 10
+found = 0
 
-while not found:
-    converging = False
+while found < attractors_to_save:
 
     # Random starting point
     x = random.uniform(-0.5, 0.5)
     y = random.uniform(-0.5, 0.5)
+
+    # Random alternative starting point nearby
+    xe = x + random.uniform(-0.5, 0.5) / 1000
+    ye = y + random.uniform(-0.5, 0.5) / 1000
+
+    # Distance between the two points
+    dx = xe - x
+    dy = ye - y
+    d0 = math.sqrt(dx * dx + dy * dy)
 
     # Random parameter vector
     a = [random.uniform(-2, 2) for _ in range(12)]
@@ -17,7 +29,11 @@ while not found:
     x_path = [x]
     y_path = [y]
 
-    for _ in range(10000):
+    # Flag to check if the path converges to a single point and lyapunov exponent
+    converging = False
+    lyapunov = 0
+
+    for i in range(10000):
         # Compute next point (using the quadratic map)
         x_new = a[0] + a[1] * x + a[2] * x * x + a[3] * y + a[4] * y * y + a[5] * x * y
         y_new = a[6] + a[7] * x + a[8] * x * x + a[9] * y + a[10] * y * y + a[11] * x * y
@@ -32,6 +48,26 @@ while not found:
             converging = True
             break
 
+        # Check for chaotic behavior (wait for 1000 iterations)
+        if i > 1000:
+            # Compute next alternative point (using the quadratic map)
+            xe_new = a[0] + a[1] * xe + a[2] * xe * xe + a[3] * ye + a[4] * ye * ye + a[5] * xe * ye
+            ye_new = a[6] + a[7] * xe + a[8] * xe * xe + a[9] * ye + a[10] * ye * ye + a[11] * xe * ye
+
+            # Compute distance between the two points
+            dx = xe_new - x_new
+            dy = ye_new - y_new
+            d = math.sqrt(dx * dx + dy * dy)
+
+            # Lyapunov exponent
+            lyapunov += math.log(abs(d / d0))
+
+            # Rescale the alternative point
+            # Random alternative starting point nearby
+            xe = x_new + d0 * dx/d
+            ye = y_new + d0 * dy/d
+
+
         # Update (x, y)
         x = x_new
         y = y_new
@@ -40,11 +76,13 @@ while not found:
         x_path.append(x)
         y_path.append(y)
 
-    if not converging:
-        found = True
+    # If chaotic behavior is found
+    if not converging and lyapunov >= 10: # if lyapunov >= 10, then the path is chaotic
+        found += 1
 
-plt.scatter(x_path, y_path, s=0.1)
-plt.show()
+        print(f"Found attractor with lyapunov exponent: {lyapunov}")
+        plt.scatter(x_path, y_path, s=0.1)
+        plt.show()
 
-plt.plot(x_path, y_path, "-o", linewidth=1, markersize=1)
-plt.show()
+# plt.plot(x_path, y_path, "-o", linewidth=1, markersize=1)
+# plt.show()
